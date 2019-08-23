f2bf902de8e7ea27867bfe2c4db5dabcf9ee7a42
dggomezmtz <48538113+dggomezmtz@users.noreply.github.com>
2019-08-22 19:04:25 -0500

    add lib-rqlite

--- Salon/src/core/db/rqlite/RQLiteConnection.java
+++ Salon/src/core/db/rqlite/RQLiteConnection.java
@@ -28,7 +28,7 @@ public class RQLiteConnection {
     private static RQLiteConnection instance;
 
     private RQLiteConnection() {
-        this.ip = "10.0.5.182";
+        this.ip = "192.168.0.118";
         this.port = 4001;
     }
 
--- Salon/src/core/gui/admon/ClassroomInter.java
+++ Salon/src/core/gui/admon/ClassroomInter.java
@@ -7,8 +7,12 @@ package core.gui.admon;
 
 import core.controller.MainController;
 import core.data.Classroom;
+import core.data.MessageACL;
 import core.data.Session;
 import core.data.User;
+import core.data.YellowPage;
+import core.queue.QueueConfig;
+import core.queue.QueueEventWriter;
 import core.utils.FileTreeModel;
 import core.utils.MyFile;
 import core.webservices.FileUtils;
@@ -20,8 +24,11 @@ import java.util.ArrayList;
 import core.webservices.OpenFileService;
 import java.awt.ComponentOrientation;
 import java.awt.Toolkit;
+import java.util.Calendar;
+import java.util.Date;
 import javax.swing.Box;
 import javax.swing.tree.TreePath;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -33,6 +40,7 @@ public class ClassroomInter extends javax.swing.JFrame {
     private User user = new User();
     private String path;
     private Classroom classroom;
+    private ArrayList<Session> sessionToday;
 
     /**
      * Creates new form Classroom
@@ -45,10 +53,10 @@ public class ClassroomInter extends javax.swing.JFrame {
         jMenuBar1.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         this.user = user;
         panelTab.setTitleAt(0, "Archivos");
-        menuUser.setText(user == null ? "" : user.getName() + "  ");  
-        FileUtils.createPath(this.classroom.getLocation()+"/"+user.getUsername());
-        path = this.classroom.getLocation()+"/"+user.getUsername();
-        root = new File(this.classroom.getLocation()+"/"+user.getUsername());
+        menuUser.setText(user == null ? "" : user.getName() + "  ");
+        FileUtils.createPath(this.classroom.getRootFolder() + "/" + user.getUsername());
+        path = this.classroom.getRootFolder() + "/" + user.getUsername();
+        root = new File(this.classroom.getRootFolder() + "/" + user.getUsername());
         // Create a TreeModel object to represent our tree of files
         FileTreeModel model = new FileTreeModel(root);
         treeDirectory.setModel(model);
@@ -62,20 +70,19 @@ public class ClassroomInter extends javax.swing.JFrame {
     public void toProjectSession() {
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime firstDate = LocalDateTime.now().minusMinutes(5);
+        LocalDateTime firstDate = LocalDateTime.now().minusMinutes(10);
         LocalDateTime lastDate = LocalDateTime.now().plusHours(2);
 
-        ArrayList<Session> sessionToday = MainController.getSessionsDates(user.getId(), dtf.format(firstDate), dtf.format(lastDate));
+        sessionToday = MainController.getSessionsDates(user.getId(), dtf.format(firstDate), dtf.format(lastDate));
 
         for (Session s : sessionToday) {
+            System.out.println(path + "/" + s.getFile());
 
-            System.out.println(path+"/"+s.getFile());
-
-            File file = new File(path+"/"+s.getFile());
+            File file = new File(path + "/" + s.getFile());
             if (file.exists()) {
-                OpenFileService.openFile(path+"/"+s.getFile());
+                OpenFileService.openFile(path + "/" + s.getFile());
             } else {
-                setlogTextArea("<html>El siguiente archivo no se encontró: <br>" + path+"/"+s.getFile() + "</html>");
+                setlogTextArea("<html>El siguiente archivo no se encontró: <br>" + path + "/" + s.getFile() + "</html>");
             }
         }
         if (sessionToday.size() == 0) {
@@ -88,6 +95,43 @@ public class ClassroomInter extends javax.swing.JFrame {
             instance = new ClassroomInter(user, classroom);
         }
         return instance;
+    }
+
+    private void CheckModifyToFiles() {
+
+        Date currentDate = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(currentDate);
+        cal.add(Calendar.MINUTE, -90);
+        Date alertDate = cal.getTime();
+
+        for (Session s : sessionToday) {
+//            System.out.println(path + "/" + s.getFile());
+            File file = new File(path + "/" + s.getFile());
+            Date modifiedDate = null;
+            if (file.exists()) {
+                modifiedDate = new Date(file.lastModified());
+
+                if (modifiedDate != null && modifiedDate.before(alertDate)) {
+//        return true;JSONObject cont = new JSONObject();
+                    MessageACL msgACL = new MessageACL();
+                    msgACL.setPerformative(msgACL.INFORM_IF);
+                    msgACL.setSender(classroom.getLocation());
+                    msgACL.setReceiver("multicast");
+                    msgACL.setTypeSender(YellowPage.SALON);
+                    msgACL.setOntology(msgACL.UPDATE);
+                    
+                    JSONObject cont = new JSONObject();
+                    cont.put("idSession", s.getId());
+                    cont.put("fileName", s.getFile());
+                    cont.put("size", file.lastModified());
+                    
+                    msgACL.setContent(cont.toJSONString());                    
+                    
+                    new QueueEventWriter(QueueConfig.ADDRESS, Integer.parseInt(classroom.getPort())).writeToQueue(msgACL.toJSONString());
+                }
+            }
+        }
     }
 
     /**
@@ -115,6 +159,11 @@ public class ClassroomInter extends javax.swing.JFrame {
         setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/core/images/class.png")));
         setPreferredSize(new java.awt.Dimension(883, 750));
         setSize(new java.awt.Dimension(883, 750));
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+        });
         getContentPane().setLayout(new java.awt.BorderLayout(0, 3));
 
         panelLog.setPreferredSize(new java.awt.Dimension(651, 125));
@@ -201,6 +250,8 @@ public class ClassroomInter extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void itemExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemExitActionPerformed
+        System.out.println("---2");
+        CheckModifyToFiles();
         System.exit(0);
     }//GEN-LAST:event_itemExitActionPerformed
 
@@ -209,7 +260,8 @@ public class ClassroomInter extends javax.swing.JFrame {
     }//GEN-LAST:event_itemToProyectActionPerformed
 
     private void treeDirectoryMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_treeDirectoryMouseClicked
-        MyFile mfile; String path;
+        MyFile mfile;
+        String path;
         int selRow = treeDirectory.getRowForLocation(evt.getX(), evt.getY());
         TreePath selPath = treeDirectory.getPathForLocation(evt.getX(), evt.getY());
         if (selRow != -1) {
@@ -231,6 +283,12 @@ public class ClassroomInter extends javax.swing.JFrame {
         Object node = treeDirectory.getLastSelectedPathComponent();// TODO add your handling code here:
 
     }//GEN-LAST:event_treeDirectoryMouseClicked
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        // TODO add your handling code here:
+        CheckModifyToFiles();
+        System.out.println("---1");
+    }//GEN-LAST:event_formWindowClosing
 
     /**
      * @param args the command line arguments
